5e0131afd9d752da67a0c3a686f94ef00c0e3631
anvaka <anvaka@gmail.com>
2015-10-12 00:21:53 -0700

    Further simplified

--- layout.js
+++ layout.js
@@ -4,15 +4,16 @@
  *
  * Each 60th iteration is saved to a file
  */
+var save = require('ngraph.tobinary');
 console.log('Parsing graph file...');
 require('./lib/loadGraph.js')(performLayout);
 
 function performLayout(graph) {
   console.log('Graph parsed. Found ' + graph.getNodesCount() + ' nodes and ' + graph.getLinksCount() + ' edges');
-  return;
   var layout = require('ngraph.offline.layout')(graph);
   console.log('Starting layout. This will take a while...');
   layout.run();
-  console.log('Done. Now export this to binary format:');
-  console.log('node toBinary.js');
+  console.log('Layout completed. Saving to binary format');
+  save(graph, { outDir: './data' });
+  console.log('Done.');
 }
--- toBinary.js
+++
@@ -1,3 +0,0 @@
-var graph = require('./lib/loadGraph.js')();
-var save = require('ngraph.tobinary');
-save(graph, { outDir: './data' });
