ae3feb57d1e32c18503ebd7c12ac593281b865a9
anvaka <anvaka@gmail.com>
2015-10-11 22:25:46 -0700

    Updated to use stream-based parser
    npm graph is too large to load it via regular JSON parser

--- layout.js
+++ layout.js
@@ -4,10 +4,15 @@
  *
  * Each 60th iteration is saved to a file
  */
-var fs = require('fs');
-var graph = require('./lib/loadGraph.js')();
-var layout = require('ngraph.offline.layout')(graph);
-console.log('Starting layout. This will take a while...');
-layout.run();
-console.log('Done. Now export this to binary format:');
-console.log('node toBinary.js');
+console.log('Parsing graph file...');
+require('./lib/loadGraph.js')(performLayout);
+
+function performLayout(graph) {
+  console.log('Graph parsed. Found ' + graph.getNodesCount() + ' nodes and ' + graph.getLinksCount() + ' edges');
+  return;
+  var layout = require('ngraph.offline.layout')(graph);
+  console.log('Starting layout. This will take a while...');
+  layout.run();
+  console.log('Done. Now export this to binary format:');
+  console.log('node toBinary.js');
+}
