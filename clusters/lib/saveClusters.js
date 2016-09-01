4c9f71590fb09a8b032ba698f7e5aee579ee9b7c
anvaka <anvaka@gmail.com>
2016-08-31 22:22:14 -0700

    Added clusters saving

---
+++ clusters/lib/saveClusters.js
@@ -0,0 +1,77 @@
+/**
+ * This code will produce a clustering file
+ */
+var coarsen = require('ngraph.coarsen');
+var path = require('path');
+var detectClusters = require('ngraph.louvain/native.js').fromNgraph;
+var toProtobuf = require('ngraph.toprotobuf');
+var ProtoBuf = require('protobufjs');
+var fs = require('fs');
+
+module.exports = saveClusters;
+
+var builder = ProtoBuf.protoFromString([
+'syntax = "proto3";',
+'',
+'message NodeCluster {',
+  'string nodeId = 1;',
+  'int32 clusterId = 2;',
+'}',
+'',
+'message Clusters {',
+'  repeated NodeCluster records = 1;',
+'}',
+].join('\n'));
+
+var Clusters = builder.build('Clusters');
+var Record = builder.build('NodeCluster');
+
+function saveClusters(graph, outDir) {
+  var currentLayer = 0;
+  var clusters;
+
+  do {
+    clusters = detectClusters(graph);
+    clusters.renumber();
+
+    console.log('Clusters found. Saving layer ' + currentLayer);
+    saveToDist(graph, clusters, currentLayer);
+
+    console.log('Coarsening the graph');
+    graph = coarsen(graph, clusters);
+    currentLayer += 1;
+  } while (clusters.canCoarse());
+
+  console.log('All done');
+
+  function saveToDist(graph, clusters, layer) {
+    var layerStr = layer.toString(10);
+    var dir = path.join(outDir, layerStr)
+    toProtobuf(graph, { outDir: dir })
+
+    var records = [];
+    graph.forEachNode(function(node) {
+      var clusterId = clusters.getClass(node.id);
+      var record = new Record();
+      record.clusterId = number(clusterId);
+      record.nodeId = node.id.toString();
+      records.push(record);
+    });
+
+    var clusters = new Clusters();
+    clusters.records = records;
+    saveProtoObject(clusters, path.join(dir, 'clusters.pb'));
+  }
+}
+
+function saveProtoObject(object, fileName) {
+  var arrayBuffer = object.toArrayBuffer();
+  // Turns out node 5.1 crashes when array buffer has length 0.
+  var nodeBuffer = (arrayBuffer.length > 0) ? new Buffer(arrayBuffer) : new Buffer(0);
+  fs.writeFileSync(fileName, nodeBuffer);
+}
+
+function number(x) {
+  var result = Number.parseInt(x);
+  return result;
+}
---
+++ clusters/saveClusters.js
@@ -0,0 +1,15 @@
+/**
+ * Converts saved graph into clusters
+ */
+console.log('Parsing graph file...');
+
+var path = require('path');
+var fileName = path.join(__dirname, '..', '..', 'byField');
+var saveClusters = require('./lib/saveClusters.js');
+
+require('../lib/loadGraph.js')(performClustering, fileName);
+
+
+function performClustering(graph) {
+  saveClusters(graph, 'data');
+}
